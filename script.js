33d8860eebaec3aaeeeeae2eda8bae40048e0c10
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-10 16:07:40 +0330

    js 3

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let combineBtn=document.querySelector('#combine-btn');
 let colorDisplay=document.querySelector('#color-display');
 let color1=document.querySelector('#color1');
 let color2=document.querySelector('#color2');
-
+let colorCode=document.querySelector('#combined-color-code');
 
 
 
 combineBtn.addEventListener('click',function(){
     colorDisplay.style.background=`linear-gradient(${color1.value},${color2.value})`;
+    colorCode.innerHTML=`${color1.value} , ${color2.value}`;
 })
